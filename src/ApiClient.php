0dc5461dc25ebdb77766a9363d5e9b23d724a311
Damien Parbhakar <damien.parbhakar@kemeo.com>
2018-10-05 16:50:19 +0200

    Adding getInstanceParams()

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -6,5 +6,22 @@ use GuzzleHttp\Client;
 
 
 class ApiClient extends Client {
+	
+	public function getInstanceParams($params) {
 
+		if(is_array($params)) {
+			$res = json_decode(($this->get('/v1/admin/instance/params'))->getBody(), true);
+
+			$data = array_values(array_filter($params,function($v){
+				return in_array($v['param'],$params);
+			}));
+
+			$data = array_combine(array_column($data,'param'), array_column($data,'valeu'));	
+		} else {
+			$res = json_decode(($this->get('/v1/admin/instance/params&name='.$params))->getBody(), true);
+			$data = array($res[0]['param'] => $res[0]['valeu']);
+		}
+
+		return $data;
+	}
 }
