ae5f1c6b2951d1663cb330f4441bf50afd00e842
Damien Parbhakar <damien.parbhakar@kemeo.com>
2018-10-11 15:59:22 +0200

    Adding X-Client-Url

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -6,13 +6,24 @@ use GuzzleHttp\Client;
 
 
 class ApiClient extends Client {
-	
+
+	public function __construct($options) {
+		parent::__construct($options);
+
+		// $res = json_decode(($this->post('/v1/token/validate'))->getBody(), true);
+		// if(!empty($res['user']) && $res['user']['matri'] > 0) {
+
+		// } else {
+
+		// }
+	}
+
 	public function getInstanceParams($params = array()) {
 
 		$data = array();
 
 		if(is_array($params)) {
-			$res = json_decode(($this->get('/v1/admin/instance/params'))->getBody(), true);	
+			$res = json_decode(($this->get('/v1/admin/instance/params'))->getBody(), true);
 
 			if(count($params) === 0) {
 				$data = $res;
@@ -20,20 +31,20 @@ class ApiClient extends Client {
 				$data = array_values(array_filter($res,function($v) use($params) {
 					return in_array($v['param'],$params);
 				}));
-			} 
+			}
 		} else {
-			$res = json_decode(($this->get('/v1/admin/instance/params?name='.$params))->getBody(), true);	
+			$res = json_decode(($this->get('/v1/admin/instance/params?name='.$params))->getBody(), true);
 			$data = $res;
 		}
 
-		$data = array_combine(array_column($data,'param'), array_column($data,'valeu'));	
+		$data = array_combine(array_column($data,'param'), array_column($data,'valeu'));
 		return $data;
 	}
 
 	public function postInstanceParams($post) {
 		$data = array();
 		$data = json_decode($this->post('/v1/admin/instance/params', array('json' => $post))->getBody(), true);
-		$data = array_combine(array_column($data,'param'), array_column($data,'valeu'));	
+		$data = array_combine(array_column($data,'param'), array_column($data,'valeu'));
 		return $data;
 	}
 
