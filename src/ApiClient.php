42da1ad7df78f641195e3d8f2938bef7ee9f9b79
Louis_TT <louis.mechitoua@kemeo.com>
2025-08-04 11:16:33 +0200

    Revert "ajout Timeout dans sendToApi"
    This reverts commit 9e7df5f8c4a1732ea4e1e85dd2769e55c34eee3e.

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -56,18 +56,13 @@ class ApiClient extends Client {
 		return $data;
 	}
 	
-    public function sendToApi( $method, $url, $params=array(), $timeout = null )
+    public function sendToApi( $method, $url, $params=array() )
     {
         $this->apiParams    = $params;
         
         $formattedFormParams = [];
         if( !empty($this->apiParams) ){
             $formattedFormParams = [ 'form_params' => $this->apiParams ];
-        }
-
-        if ($timeout !== null)
-        {
-            $formattedFormParams['timeout'] = $timeout;
         }
 
         $this->apiResult    = $this->request($method, $url, $formattedFormParams)->getBody()->getContents();
